11f52f9db08af2620e7055a9627534c33fcdde85
Ju, Minji <alswl0w0@gmail.com>
2022-07-15 15:55:38 +0900

    <WiseSaying>List 생성, 목록 기능 구현

--- src/main/java/com/ll/example/App.java
+++ src/main/java/com/ll/example/App.java
@@ -1,5 +1,7 @@
 package com.ll.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -8,6 +10,7 @@ public class App {
         System.out.println("== 명언 SSG ==");
         Scanner sc = new Scanner(System.in);
         int lastId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         outer:
         while (true) {
@@ -21,6 +24,18 @@ public class App {
                     System.out.printf("작가 : ");
                     String author = sc.nextLine().trim();
                     System.out.printf("%d번 명언이 등록되었습니다.\n",++(lastId));
+
+                    int id = lastId;
+
+                    WiseSaying wiseSaying = new WiseSaying(id,content,author);
+                    wiseSayings.add(wiseSaying);
+                    break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+                    for (WiseSaying temp : wiseSayings) {
+                        System.out.printf("%d / %s / %s\n", temp.id, temp.content, temp.author);
+                    }
                     break;
                 case "종료":
                     break outer;
