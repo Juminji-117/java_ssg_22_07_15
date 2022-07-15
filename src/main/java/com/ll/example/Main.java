4eca8697a03adf340dc86618438b607cfca00217
Ju, Minji <alswl0w0@gmail.com>
2022-07-15 15:20:41 +0900

    cmd 기능 구현: 등록 및 종료

--- src/main/java/com/ll/example/Main.java
+++ src/main/java/com/ll/example/Main.java
@@ -1,4 +1,7 @@
 package com.ll.example;
 
 public class Main {
+    public static void main(String[] args) {
+        new App().run();
+    }
 }
---
+++ src/main/java/com/ll/example/WiseSaying.java
@@ -0,0 +1,14 @@
+package com.ll.example;
+
+public class WiseSaying {
+    int id;
+    static String content;
+    String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+}
